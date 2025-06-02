2cc840f958cf42d5b6ed8e0338326b72681d9b0e
charithaDimuth <charitha.cdw@gmail.com>
2025-06-02 23:28:22 +1000

    Update index.js

--- src/index.js
+++ src/index.js
@@ -15,7 +15,4 @@ app.get("/health", (req, res) => {
   res.send("OK");
 });
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-
 module.exports = app;
